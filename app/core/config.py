ab3bc2520c72d6335e094b0b297ccdb31ad7fd31
zainokta <zainokta@gmail.com>
2025-03-13 16:27:36 +0700

    feat: integrate with openai

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     
     allowed_origins: str
 
+    openai_api_key: str
+
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 settings = Settings()
--- app/services/question.py
+++ app/services/question.py
@@ -1,8 +1,19 @@
 from app.database.database import conn, model
+from app.core.config import settings
 
+from langchain_core.globals import set_llm_cache
+from langchain_openai import ChatOpenAI
+from langchain_core.caches import InMemoryCache
 from fastapi import Depends
-from typing import Annotated  
+from typing import Annotated 
 
+llm = ChatOpenAI(
+    model="gpt-4o-mini", 
+    temperature=0.1, 
+    api_key=settings.openai_api_key
+)
+
+set_llm_cache(InMemoryCache())
 
 class QuestionService():
     def search_portfolio(self, query, top_k=3):
@@ -25,15 +36,23 @@ class QuestionService():
     def answer(self, user_query: str):
         relevant_contexts = self.search_portfolio(user_query)
         context_text = "\n\n".join(relevant_contexts)
-        
-        prompt = f"""You are a helpful assistant for my portfolio website.
-        
-        Here is relevant information from my portfolio:
-        {context_text}
-        
-        Based on this information, please answer: {user_query}"""
 
-        return user_query
+        messages = [
+        {"role": "system", "content": """You are an AI assistant that provides detailed and well-structured answers about my portfolio.
+        
+            - If the query is related to my experience, skills, or projects, extract relevant information from the provided context and elaborate on it.
+            - If multiple topics are mentioned, try to address each one clearly.
+            - If a direct answer is not available, make a logical connection based on the given context rather than saying "I cannot answer."
+            - If needed, provide insights about my contributions, challenges faced, and the impact of my work.
+            - Don't mention about from "context provided", or "from the portfolio".
+            - Act as you are my assistant answering someone who asks about my related experience, skills, or projects. Answer the question asked as the third party and calling my name "Zain".
+            """},
+            {"role": "user", "content": f"Here is relevant information from my portfolio:\n\n{context_text}\n\nBased on this, please provide a detailed response to the following query:\n\n{user_query}"}
+        ]
+
+        output = llm.invoke(messages)
+        return output.content
+
         
 question_service = QuestionService
 
